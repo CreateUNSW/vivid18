af3e8b3f62d44459b14ffbb7994bbf3dc7aeb1c9
steve <stev3bb@gmail.com>
2018-03-16 13:13:01 +1100

    half ass working decoder

--- encoder/encoder.ino
+++ encoder/encoder.ino
@@ -64,22 +64,81 @@ void setup() {
   Serial.begin(9600);
 }
 
-int decode (char input []) {
-  // takes a 4 byte brinary input and returns a 32 bit int
+DataStream decode (int input []) {
+  //need to malloc 
   // little endian or big boy?
-  //ie 0000 0000 0000 0000
-  // need to run some test to confirm
-  unsigned long result;
-  //result = input[3];
-  //result = (result<<8)|input[2];
-  //result = (result<<8)|input[1];
+  //unpack the stream into a datastruct
+  DataStream result;
+  int tempBits; 
+
+  int spiltA;
+  int spiltB;
+  //one byte goes here
+  result.addr = input[0] >> 5 & 0xff;
+  
+  //prob add an if condition here
+  if(input[0] & (1<<4)){
+    Serial.println("this is true");
+    result.updir = true;
+  }else{
+    result.updir = false;
+  }
+  
+  //might wanna make a temp bit so it will have snipets 
+  result.prior = (input[0] & ~(result.addr<<5 | result.updir<<3)) >>2 ;
+  int cutTemp = (result.addr<<5 | result.updir<<3 | result.prior <<2);
+  Serial.println(cutTemp, BIN);
+  result.pace = (input[0] & ~cutTemp); 
+  
+  /*
+  1. 111 0 10 01 
+  2. 1010 011 0
+  3. 1011 011 0
+  4. 1101 0000
+  */
+
+  //3bits and a pad + another 3 bits
+  result.eff = input[1] >> 5 & 0xff;
+  //result.lumA = ;
+
+  //need the last bit of the previous array + 5 bits
+  //resylt.colorA =;
+  //remain bits after 4th bit
+  //result.lumB =;
+
+  //need the last bit of the previous array and clear out the padding
+  //result.colorB = input[3];
+  int bitNum = 0;
+
+  //debugging code here
+  Serial.println("debug ===========================");
+  Serial.println(input[0],BIN);
+  //Serial.println()
+  //Serial.println()
+  //Serial.println()
+
+  //check the struct
+  Serial.println(result.addr);
+  //Serial.println("check");
+  //Serial.println(input[0], BIN);
+  Serial.println(result.updir);
+  Serial.println(result.prior);
+  Serial.println(result.pace);
+  Serial.println(result.eff);
+  //Serial.println(result.lumA);
+  //Serial.println(result.colorA);
+  //Serial.println(result.lumB);
+  //  Serial.println(result.colorB);
+  
+  
+  /* working model
+  long result;
   result = input[0] & 0xff;
   result = input[1] & 0xff | result<<8;
   result = input[2] & 0xff | result<<8;
   result = input[3] & 0xff | result<<8;
+  */
 
-
-  Serial.println(result);
   return result;
 }
 
@@ -101,8 +160,6 @@ int encode (DataStream memes) {
   3. 1011 011 0
   4. 1101 0000
   */
-
-  
   results[1] =  memes.eff & 0xff | results[1] << 3;
   //add padding heres
   results[1] = 0 & 0xff | results[1] << 1;
@@ -151,46 +208,56 @@ int encode (DataStream memes) {
   return *results;
 }
 
+void EncodeTest(){
+  //example input being tested:
+  ////inputs 1110 1001 1010 011 01011 011 01101 0000
+  DataStream memes;
+  
+  memes.addr = 7;
+  memes.updir = false;
+  memes.prior = 1;
+  memes.pace = 2;
+  memes.eff = 5;
+  memes.lumA = 3;
+  memes.colorA = 11;
+  memes.lumB = 3;
+  memes.colorB = 29;
 
-//helper functions
+  //prob need to think of a null terminator at the end of the stream
+  //DataStream *memesAdd = memes;
 
-void loop(){
-    //do some test here
-  /*char test1[4];
+  //temp
+  long test2;
+  test2= encode(memes);
+}
+
+void DecodeTest(){
+  int test1[4];
   test1[0] = 0b11101001;
   test1[1] = 0b10100110;
   test1[2] = 0b10110110;
   test1[3] = 0b11010000;
-  char *p = test1;
+  int *p = test1;
   decode (p);
-  */
 
-  DataStream memes;
-  //inputs 1110 1001 1010 011 01011 011 01101 0000
-  memes.addr = 7;
-  memes.updir = false;
-  
-  memes.prior = 2;
-  memes.pace = 1;
-  
-  memes.eff = 5;
-  
-  memes.lumA = 3;
-  memes.colorA = 11;
-  
-  memes.lumB = 3;
-  memes.colorB = 29;
+  /*expected serial print out result
+   * 
+   */
+}
 
-  //terminator
-  //DataStream *memesAdd = memes;
+//helper functions
+boolean validData (int stream[]){
+  //check data integrity
+  //check if the size is correct
   
-  long test2;
-  test2= encode(memes);
+  return true;
+}
 
+void loop(){
+  //run some test cases here
+  //EncodeTest();
+  DecodeTest();
   
   delay (1000);
-
-  
-  
 }
 
