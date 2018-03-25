590ffea5ddd1925aac69d4bea831e2d190e63c62
pentra <nevinsuri@hotmail.com>
2018-03-25 16:05:09 +1100

    configure virtual leds

--- hope/hope/hope.ino
+++ hope/hope/hope.ino
@@ -4,14 +4,16 @@
 
 // Crystals/LEDs config
 //--------------------------------------------------
-#define NUM_LEDS 23
-#define DATA_PIN 3
-#define LED_TYPE WS2811
-
 #define NUM_CHAINS 2
+#define LED_TYPE WS2811
+#define FastLED_ORDER RGB
+#define MAX_NUM_LEDS 50
+
+#define NUM_LEDS_1 50
+#define DATA_PIN_1 3
 //--------------------------------------------------
 
-CRGB physicalLEDs[NUM_CHAINS][NUM_LEDS];
+CRGB physicalLEDs[NUM_CHAINS][MAX_NUM_LEDS];
 
 class Effect;
 class LED;
@@ -21,7 +23,7 @@ class Chain;
 class Effect {
 public:
   virtual ~Effect() {}
-  virtual void draw(long unsigned int currentTime, Chain& chain) = 0;
+  virtual void draw(long unsigned int currentTime, Chain* chain) = 0;
 };
 
 struct _LedPosition {
@@ -44,6 +46,7 @@ public:
 		ledPosition = (LedPosition*)malloc(numLEDs * sizeof(LedPosition));
 	}
 
+	// map this virtual led to another physical led
 	void addPhysicalLED(char chainNum, char ledNum)
 	{
 		ledPosition[num].chainIndex = chainNum;
@@ -56,10 +59,13 @@ public:
 		this->val = inValue;
 	}
 
-	void displayLED()
+	// copy virtual led value to all physical led mapped values
+	// TODO: maybe use physical LED array to store value?
+	void writeLED()
 	{
 		for (int i = 0; i < num; i++)
 			physicalLEDs[ledPosition[i].chainIndex][ledPosition[i].ledIndex] = val;
+		//physicalLEDs[ledPosition[0].chainIndex][ledPosition[0].ledIndex] += CHSV(1, 1, 1);
 	}
 };
 
@@ -83,31 +89,33 @@ public:
 	void executeEffects() {
 		//Serial.println("executing");
 		//Serial.flush();
-		int size = leds.size();
-		for (int i = 0; i < size; i++) {
-			//colors.push_back(&(CHSV(0, 0, 0)));
-		}
 		for (auto effect : effects) {
-			effect->draw(millis(), *this);
+			effect->draw(millis(), this);
 		}
 		//Serial.println("finished exec");
 		//Serial.flush();
 	};
 
-	// write color values to LEDs
+	// Read a config array
+	void setConfiguration(char** config) {
+		for (int i = 0; i < leds.size(); i++) {
+			int size = config[i][0];
+			for (int j = 0; j < size; j++) {
+				leds.at(i).addPhysicalLED(config[i][1 + 2 * j], config[i][1 + 2 * j + 1]);
+			}
+		}
+	}
+
+	// write color values from virtual LEDs to physical
 	void displayLEDs()
 	{
 		//Serial.println(String((*(colors[0])).hue));
 		//FastLED.clear();
-		for (int i = 0; i < 23; i++)
-		{
-			leds[i].displayLED();
-		}
 
-		/*for (auto led : leds)
+		for (auto led : leds)
 		{
-			led.displayLED();
-		}*/
+			led.writeLED();
+		}
 		FastLED.show();
 	};
 };
@@ -121,13 +129,13 @@ public:
 	int speed;
 	fract8 fractspeed;
 
-	virtual void draw(long unsigned int currentTime, Chain& chain) {
+	virtual void draw(long unsigned int currentTime, Chain* chain) {
 		// Serial.println("begin draw");
 		//Serial.flush();
-		for (auto color : chain.leds) {
-			toColor.hue > color.val.hue ? color.val.hue += speed : 1;
-			toColor.sat > color.val.sat ? color.val.sat += speed : 1;
-			toColor.val > color.val.val ? color.val.val += speed : 1;     
+		for (auto led : chain->leds) {
+			toColor.hue > led.val.hue ? led.val.hue += speed : 1;
+			toColor.sat > led.val.sat ? led.val.sat += speed : 1;
+			toColor.val > led.val.val ? led.val.val += speed : 1;     
 		}
 		//Serial.println("COLORS:");
 		//Serial.println(String(toColor.val));
@@ -137,37 +145,53 @@ public:
 	};
 };
 
+void configLEDs()
+{
+	// Add physical LEDs to FastLED
+	FastLED.addLeds<LED_TYPE, DATA_PIN_1, FastLED_ORDER>(physicalLEDs[0], NUM_LEDS_1);
+
+	// initialize virtual chains
+	Chain chain = Chain(2);
+	// For each virtual led, give a list of physical leds (chain, led) that it maps to) 
+	// (first element indicates how many physical leds that led maps to)
+	const int numVirtualLEDs = 2;
+	const int maxLEDs = 2;
+	char config1[numVirtualLEDs][maxLEDs*2 + 1] = 
+													{ {2, 0,0, 1,0},
+													{2, 0,1, 1,1} };
+	chain.setConfiguration((char**)config1);
+	chains.push_back(chain);
+}
+
 void setup() {
 	Serial.begin(9600);
 	Serial.println("boot");
 	delay(1000);
 
-	FastLED.addLeds<LED_TYPE, DATA_PIN, RGB>(physicalLEDs[0], NUM_LEDS); 
+	configLEDs();
 
-	// initialize one chain with 23 LEDs
-	Chain chain = Chain(NUM_LEDS);
+	delay(1000);
+	Serial.println("finished setup");
+	Serial.flush();
+
+	// Just for testing:
 
 	// Add fade effect to chain
 	Fade* fade = new Fade();
 	fade->toColor = CHSV(255, 255, 255);
 	fade->speed = 10;
 	fade->fractspeed = 0.01f;
-  
-	chain.effects.push_back(fade);
 
-	chains.push_back(chain); 
-	delay(1000);
-	Serial.println("finished setup");
-	Serial.flush();
+	chains[0].effects.push_back(fade);
 }
 
 void loop() {
 	unsigned long start = micros();
 	for (auto chain : chains) {
-	chain.executeEffects();
-	chain.displayLEDs();
-	//Serial.println("chaining");
-	//Serial.flush();
+		chain.executeEffects();
+		chain.displayLEDs();
+		//Serial.println("chaining");
+		//Serial.flush();
 	}
 	unsigned long end = micros();
 	unsigned long delta = end - start;
